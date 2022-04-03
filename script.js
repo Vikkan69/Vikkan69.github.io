e91809e918d856ab7879db6bb78890c6ed663939
Viktor Costa <viktorcosta200@hotmail.com>
2022-04-03 23:52:00 +0200

    Added pictures

--- script.js
+++ script.js
@@ -3,21 +3,19 @@ window.onbeforeunload = () => {
   alert("forget it");
 };
 
+let url = "https://youtu.be/xvFZjo5PgG0?autoplay=1";
+
+// openTab();
+
+function openTab() {
+  setTimeout(() => {
+    window.open(url);
+    openTab();
+  }, 100);
+}
+
 box.addEventListener("click", () => {
   alert("You have been Rick Rolled");
-  console.log("Hej");
-
-  let url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
-
-  openTab();
-
-  function openTab() {
-    setTimeout(() => {
-      window.open(url);
-      console.log("Opening tab...");
-      openTab();
-    }, 100);
-  }
 });
 
 console.log("Hej");
